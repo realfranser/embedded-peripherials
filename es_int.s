fbf50062a6dbd557bf01de0841fb7c0b59563a2e
realfranser <serranoarresejavier@gmail.com>
2021-05-03 20:01:47 +0200

    Scan semi-tested, print no tested

--- es_int.s
+++ es_int.s
@@ -73,8 +73,8 @@ IVR		EQU		$EFFC19
                 MOVE.B    #%00100010,IMRCOPY    * Copiar IMR en nuestra propia variable
                 MOVE.B    IMRCOPY,IMR
 
-                MOVE.L    #RTI,$100             * Decimal: 4*64 = 256 -> Hexa: 100
                 MOVE.B    #$40,IVR              * Vector de interrupcion = 64
+                MOVE.L    #RTI,$100             * Decimal: 4*64 = 256 -> Hexa: 100
 
 
                 * Mis propios inits
@@ -86,25 +86,26 @@ IVR		EQU		$EFFC19
                 MOVE.L    #BUFF_2,8(A0)         * Buffer transmision A
                 MOVE.L    #BUFF_3,12(A0)        * Buffer transmision B
 
-                MOVE.L    #BUFF_0,A0
+                * Inicializo todos los punteros de los buffers
+                MOVE.L    #BUFF_0,A0            * Recepcion A
                 MOVE.L    #BUFF_0+16,(A0)
                 MOVE.L    #BUFF_0+16,4(A0)
                 MOVE.L    #BUFF_0+16,8(A0)
                 MOVE.L    #BUFF_0+SIZE+16,12(A0)
 
-                MOVE.L    #BUFF_1,A0
+                MOVE.L    #BUFF_1,A0            * Recepcion B
                 MOVE.L    #BUFF_1+16,(A0)
                 MOVE.L    #BUFF_1+16,4(A0)
                 MOVE.L    #BUFF_1+16,8(A0)
                 MOVE.L    #BUFF_1+SIZE+16,12(A0)
 
-                MOVE.L    #BUFF_2,A0
+                MOVE.L    #BUFF_2,A0            * Transmision A
                 MOVE.L    #BUFF_2+16,(A0)
                 MOVE.L    #BUFF_2+16,4(A0)
                 MOVE.L    #BUFF_2+16,8(A0)
                 MOVE.L    #BUFF_2+SIZE+16,12(A0)
 
-                MOVE.L    #BUFF_3,A0
+                MOVE.L    #BUFF_3,A0            * Transmision B
                 MOVE.L    #BUFF_3+16,(A0)
                 MOVE.L    #BUFF_3+16,4(A0)
                 MOVE.L    #BUFF_3+16,8(A0)
@@ -113,9 +114,12 @@ IVR		EQU		$EFFC19
 
                 RTS
 
-
-
-
+    *██╗     ███████╗███████╗ ██████╗ █████╗ ██████╗ *
+  ***██║     ██╔════╝██╔════╝██╔════╝██╔══██╗██╔══██╗***
+*****██║     █████╗  █████╗  ██║     ███████║██████╔╝*****
+*****██║     ██╔══╝  ██╔══╝  ██║     ██╔══██║██╔══██╗*****
+  ***███████╗███████╗███████╗╚██████╗██║  ██║██║  ██║***
+    *╚══════╝╚══════╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝*
 
     LEECAR:     MOVEM.L A0-A2,-(A7)
                 AND.L   #$3,D0
@@ -145,8 +149,12 @@ IVR		EQU		$EFFC19
     LC_END:     MOVEM.L (A7)+,A0-A2     * Fin leecar
                 RTS
 
-
-
+    *███████╗███████╗ ██████╗ ██████╗ █████╗ ██████╗ *
+  ***██╔════╝██╔════╝██╔════╝██╔════╝██╔══██╗██╔══██╗***
+*****█████╗  ███████╗██║     ██║     ███████║██████╔╝*****
+*****██╔══╝  ╚════██║██║     ██║     ██╔══██║██╔══██╗*****
+  ***███████╗███████║╚██████╗╚██████╗██║  ██║██║  ██║***
+    *╚══════╝╚══════╝ ╚═════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝*
 
     ESCCAR:     MOVEM.L A0-A2,-(A7)
                 AND.L   #$3,D0
@@ -167,51 +175,125 @@ IVR		EQU		$EFFC19
                 * No esta lleno
                 MOVE.L  A2,4(A0)
                 MOVE.L  #0,D0
-                BRA     FINEC
+                BRA     EC_END
 
     FULL:       MOVE.L  #-1,D0
 
-    FINEC:      MOVEM.L (A7)+,A0-A2
+    EC_END:      MOVEM.L (A7)+,A0-A2
                 RTS
+
+    *███████╗ ██████╗ █████╗ ███╗   ██╗*
+  ***██╔════╝██╔════╝██╔══██╗████╗  ██║***
+*****███████╗██║     ███████║██╔██╗ ██║*****
+*****╚════██║██║     ██╔══██║██║╚██╗██║*****
+  ***███████║╚██████╗██║  ██║██║ ╚████║***
+    *╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝*
+
+    SCAN:       LINK    A6,#0           * Los parametros de entrada estan 8B detras del puntero de pila
+                MOVE.L  8(A6),A1        * A1 -> primer param (direccion del buffer destino)
+
+                EOR.L   D0
+                MOVE.W  12(A6),D0       * D0 -> segundo param (descriptor del dispositivo sobre el que se lee)
+
+                EOR.L   D1
+                MOVE.W  14(A6),D1       * D1 -> tercer param (size, num max de caracteres a copiar)
+
+                EOR.L   D2              * D2 -> retorno (contador de carcteres leidos)
+
+                ***Seccion de deteccion de errores***
+                CMP.W   #0,D1           * Si el size < 0 -> error (absurdo!)
+                BLT     SC_ERR
+                CMP.W   #1,D0           * Si el desc > 1 -> buffer desc incorrecto
+                BGT     SC_ERR
+                CMP.W   #0,D0           * Si el desc < 0 -> buffer desc incorrecto
+                BLT     SC_ERR
+
+    SC_BUF:     CMP.W   #0,D1
+                BEQ     SC_OK           * Comprobamos si se han leido todos los caracteres
+                MOVE.W  12(A6),D0        * Reseteamos el desc. del dispositivo sobre el que leer
+                BSR     LEECAR
+                CMP.L   #-1,D0          * Si empty buffer -> se va al final
+                BEQ     SC_OK
+                MOVE.B  D0,(A1)+        * Copiamos el caracter devuelto por leecar en la pos del buffer pasada
+                SUB.W   #1,D1           * Size --
+                ADD.L   #1,D2           * Contador ++
+                BRA     SC_BUF
+
+    SC_ERR:     MOVE.L  #-1,D0
+                BRA     SC_END
+
+    SC_OK:      MOVE.L  D2,D0
+
+    SC_END:     UNLK    A6
+                RTS
+
+    *██████╗ ██████╗ ██╗███╗   ██╗████████╗*
+  ***██╔══██╗██╔══██╗██║████╗  ██║╚══██╔══╝***
+*****██████╔╝██████╔╝██║██╔██╗ ██║   ██║   *****
+*****██╔═══╝ ██╔══██╗██║██║╚██╗██║   ██║   *****
+  ***██║     ██║  ██║██║██║ ╚████║   ██║   ***
+    *╚═╝     ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝   ╚═╝   *
+
+    PRINT:      LINK  A6,#0             * Los parametros de entrada estan 8B detras del puntero de pila
+                MOVE.L  8(A6),A1        * A1 -> primer param (direccion del buffer destino)
+
+                EOR.L   D0
+                MOVE.W  12(A6),D0       * D0 -> segundo param (descriptor del dispositivo sobre el que se lee)
+                * No se debe usar D1 ya que es un parametro de ESCCAR
+                EOR.L   D2 * Puede que no haga falta este
+                MOVE.W  14(A6),D2       * D2 -> tercer param (size, num max de caracteres a copiar)
+
+                EOR.L   D3,D3           * D3 -> retorno (contador de carcteres escritos)
+
+                ***Seccion de deteccion de errores***
+                CMP.W   #0,D2           * Si el size < 0 -> error (absurdo!)
+                BLT     PR_ERR
+                CMP.W   #1,D0           * Si el desc > 1 -> buffer desc incorrecto
+                BGT     PR_ERR
+                CMP.W   #0,D0           * Si el desc < 0 -> buffer desc incorrecto
+                BLT     PR_ERR
+
+
+    PR_BUF:     CMP.W   #0,D2
+                BEQ     PR_MTX
+                MOVE.W  12(A6),D0       * Reestablecemos el desc. del dispositivo sobre el que escribir
+                ADD.W   #2,D0           * Para obtener el descriptor del buffer interno 2 = trans A, 3 = trans B
+                EOR.L   D1,D1 * Puede que esta linea sobre
+                MOVE.B  (A1)+,D1
+                BSR     ESCCAR          * Params: D0 -> desc (2+X para X = 0 e 1), D1 -> buff pointer
+                CMP.L   #-1,D0          * Si empty buffer -> se va al final
+                BEQ     PR_MTX
+                SUB.W   #1,D2           * Size --
+                ADD.L   #1,D3           * Contador ++
+                BRA     PR_BUF
+
+    PR_MTX:     CMP.L   #0,D3
+                BEQ     PR_OK           * Si no se escribe nada, no es necesario hacer gestion mutex
+                MOVE.W  SR,D4           * Salvamos SR (status register)
+                * Puede crearse una variable global con valor $2700
+                MOVE.W  #$2700,SR       *  Inhibir interrupciones
+                * Para dejar en T Ready la linea A, necesitamos activar el bit 0
+                * Para dejar en T Ready la linea B, necesitamos activar el bit 4
+                * Si multiplicamos 4 por el descriptor de entrada (0 e 1) tenemos el bit que deseamos de IMR
+                MOVE.W  12(A6),D2
+                MULU    #4,D2
+                BSET    D2,IMR
+                BSET    D2,IMRCOPY           
+                MOVE.W  D4,SR           * Restauramos SR (status register)
+                MOVE.L  D3,D0           * D3 (ret -> num car escritos) MV a D0
+                BRA     PR_END
+
+    PR_ERR:     MOVE.L  #-1,D0
+
+    PR_END:     UNLK    A6
+                RTS
+              
 
 
     RTI:        RTE
 
 
 
-*	PPAL:		MOVE.L #BUS_ERROR,8 * Bus error handler
-*				MOVE.L #ADDRESS_ER,12 * Address error handler
-*				MOVE.L #ILLEGAL_IN,16 * Illegal instruction handler
-*				MOVE.L #PRIV_VIOLT,32 * Privilege violation handler
-*				MOVE.L #ILLEGAL_IN,40 * Illegal instruction handler
-*				MOVE.L #ILLEGAL_IN,44 * Illegal instruction handler
-*				
-*				BSR INIT
-*				
-*				MOVE.L 	#1,D0	*D0=1 (Rx Línea B)
-*				MOVE.B  #$41,D1
-*				BSR 	ESCCAR
-*				
-*				MOVE.L 	#1,D0
-*				BSR 	LEECAR	*Si todo va bien, en D0 deberíamos tener $41
-*				
-*				MOVE.L 	#2002,D2
-*	BUCLE:		MOVE.L 	#1,D0	*D0=1 (Rx Línea B)
-*				MOVE.B  #$41,D1
-*				BSR 	ESCCAR
-*				SUB.L 	#1,D2	
-*				CMP.L 	#0,D2
-*				BNE		BUCLE	*Cuando salga del bucle, el valor de D0 debería ser -1, pq ya está lleno el buffer
-*				
-*	BUS_ERROR: 	BREAK * Bus error handler
-*				NOP
-*	ADDRESS_ER: BREAK * Address error handler
-*				NOP
-*	ILLEGAL_IN: BREAK * Illegal instruction handler
-*				NOP
-*	PRIV_VIOLT: BREAK * Privilege violation handler
-*				NOP
-				
 
 PPAL: * Manejadores de excepciones
     MOVE.L  #BUS_ERROR,8     * Bus error handler
@@ -220,16 +302,16 @@ PPAL: * Manejadores de excepciones
     MOVE.L  #PRIV_VIOLT,32   * Privilege Violation handler
 
     BSR     INIT
-*
-*
-***************************************************
-**██╗     ███████╗███████╗ ██████╗ █████╗ ██████╗ *
-**██║     ██╔════╝██╔════╝██╔════╝██╔══██╗██╔══██╗*
-**██║     █████╗  █████╗  ██║     ███████║██████╔╝*
-**██║     ██╔══╝  ██╔══╝  ██║     ██╔══██║██╔══██╗*
-**███████╗███████╗███████╗╚██████╗██║  ██║██║  ██║*
-**╚══════╝╚══════╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝*
-**************************************************
+
+***************************************
+*██╗  ██╗██╗████████╗ ██████╗      ██╗*
+*██║  ██║██║╚══██╔══╝██╔═══██╗    ███║*
+*███████║██║   ██║   ██║   ██║    ╚██║*
+*██╔══██║██║   ██║   ██║   ██║     ██║*
+*██║  ██║██║   ██║   ╚██████╔╝     ██║*
+*╚═╝  ╚═╝╚═╝   ╚═╝    ╚═════╝      ╚═╝*
+***************************************
+                                     
 
 *** Prueba 2: Llamada a ESCCAR introduciendo un caracter en el buffer de recepción de la línea A (0)
 ***
@@ -351,15 +433,6 @@ PPAL: * Manejadores de excepciones
 *    BRA FINC
 
 
-**************************************************
-*███████╗███████╗ ██████╗ ██████╗ █████╗ ██████╗ *
-*██╔════╝██╔════╝██╔════╝██╔════╝██╔══██╗██╔══██╗*
-*█████╗  ███████╗██║     ██║     ███████║██████╔╝*
-*██╔══╝  ╚════██║██║     ██║     ██╔══██║██╔══██╗*
-*███████╗███████║╚██████╗╚██████╗██║  ██║██║  ██║*
-*╚══════╝╚══════╝ ╚═════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝*
-**************************************************
-
 *************************************************************************************************************************
 **
 *** Prueba 11: Se realiza la inserción de 2000 caracteres en el buffer interno de transmisión de la linea B(3) llamando ***
@@ -476,6 +549,66 @@ PPAL: * Manejadores de excepciones
 *    MOVE.L #-1,D5
 *    BRA FINC
 
+
+************************************
+*███████╗ ██████╗ █████╗ ███╗   ██╗*
+*██╔════╝██╔════╝██╔══██╗████╗  ██║*
+*███████╗██║     ███████║██╔██╗ ██║*
+*╚════██║██║     ██╔══██║██║╚██╗██║*
+*███████║╚██████╗██║  ██║██║ ╚████║*
+*╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝*
+************************************
+
+*** Prueba scan 1: inserta 100 caracteres en el buffer interno de recepcion de la linea A (0)
+*** se leen 100. Resultado esperado -> D0 = 64 (100 en hex)
+
+p_scan_1:
+    MOVE.L  #$100,D3
+    MOVE.L  #0,D7
+    BSR     BUCESNFF
+    MOVE.W  #$100,-(A7)
+    MOVE.W  #0,-(A7)
+    MOVE.L  #$5000,-(A7)
+    BSR     SCAN
+    CMP.L   #$100,D0
+    BNE     AMENTET
+
+*** Prueba scan 2: inserta 100 caracteres en el buffer interno de recepcion de la linea B (1)
+*** se leen 25 caracteres 4 veces de tal manera que el buffer quede vacio. Resultado esperado:
+*** D0 = 19 (25 en hex). Pos $50FF en memoria = pos $D01
+                                  
+p_scan_2:
+            MOVE.L  #$94,D3         * 25 * 4 es 94 en hex
+            MOVE.L  #1,D7
+            BSR    BUCESNFF
+            MOVE.L  #0,D4
+p_s_2_b:    CMP.L   #4,D4
+            BEQ     p_s_2_e
+            MOVE.W  #$25,-(A7)
+            MOVE.W  #1,-(A7)
+            MOVE.L  #$5000,-(A7)
+            BSR     SCAN
+            CMP.L   #$25,D0
+            BNE     AMENTET
+            ADD.L   #1,D4
+            BRA     p_s_2_b
+p_s_2_e:    MOVE.L  D3,D4
+
+
+*** Prueba scan 3: inserta 10 caracteres y se intenta leer 20 en el buffer interno de recepcion A (0)
+*** El resultado esperado es 10 en D0 y guardar desde 00 hasta 09 en la posicion $5000 de memoria
+p_scan_3:   MOVE.L  #10,D3
+            MOVE.L  #0,D7
+            BSR     BUCESNFF
+            MOVE.W  #20,-(A7)
+            MOVE.W  #0,-(A7)
+            MOVE.L  #$5000,-(A7)
+            BSR     SCAN
+            CMP.L   #10,D0
+            BNE     AMENTET
+            MOVE.L  D3,D4
+
+
 *********************************************************
 * █████╗ ██╗   ██╗██╗  ██╗██╗██╗     ██╗ █████╗ ██████╗ *
 *██╔══██╗██║   ██║╚██╗██╔╝██║██║     ██║██╔══██╗██╔══██╗*
@@ -484,6 +617,27 @@ PPAL: * Manejadores de excepciones
 *██║  ██║╚██████╔╝██╔╝ ██╗██║███████╗██║██║  ██║██║  ██║*
 *╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝╚══════╝╚═╝╚═╝  ╚═╝╚═╝  ╚═╝*
 ********************************************************
+
+* La rutina AMENTET hace referencia a la diosa egipcia homonima la cual era la patrona de las puertas
+* del inframundo en el que esperaba a los difuntos que no superaban las pruebas en su camino al paraiso.
+* En este caso, las subrutinas que no superen las pruebas, las esperara AMENTET que para indicar su fallo
+* y mandarlas corregir.
+AMENTET:
+            MOVE.L  #-1,D0
+            MOVE.L  #-1,D1
+            MOVE.L  #-1,D2
+            MOVE.L  #-1,D3
+            MOVE.L  #-1,D4
+            MOVE.L  #-1,D5
+            MOVE.L  #-1,D6
+            MOVE.L  #-1,D7
+            MOVE.L  #-1,A0
+            MOVE.L  #-1,A1
+            MOVE.L  #-1,A2
+            MOVE.L  #-1,A3
+            MOVE.L  #-1,A4
+            MOVE.L  #-1,A5
+            BREAK
                                                        
 *************************************************************************************************************************
 *****
